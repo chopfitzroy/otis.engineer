4a92e157a95c908a5a21988f23192c6c38be59ed
Otis Sutton <chopfitzroy@gmail.com>
2026-02-26 17:39:34 +1300

    Add new pages and improve structure

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ set -eu
 
 PANDOC_FLAGS="--standalone --from=asciidoc --template=templates/page.html --css=style.css"
 
-pandoc $PANDOC_FLAGS --variable nav-overview=true --output=index.html content/index.adoc
-pandoc $PANDOC_FLAGS --variable nav-tabletop=true --output=tabletop.html content/tabletop.adoc
-pandoc $PANDOC_FLAGS --variable nav-software=true --output=software.html content/software.adoc
-pandoc $PANDOC_FLAGS --variable nav-recipes=true --output=recipes.html content/recipes.adoc
+pandoc $PANDOC_FLAGS --variable nav-overview=true --output=public/index.html content/index.adoc
+pandoc $PANDOC_FLAGS --variable nav-tabletop=true --output=public/tabletop.html content/tabletop.adoc
+pandoc $PANDOC_FLAGS --variable nav-software=true --output=public/software.html content/software.adoc
+pandoc $PANDOC_FLAGS --variable nav-recipes=true --output=public/recipes.html content/recipes.adoc
+pandoc $PANDOC_FLAGS --variable nav-links=true --output=public/links.html content/links.adoc
+pandoc $PANDOC_FLAGS --variable nav-books=true --output=public/books.html content/books.adoc
